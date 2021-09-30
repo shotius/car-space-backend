884f6cdd41dbe9b7becf7f447b7ad98375270bed
shotius <sh.archemashvili@gmail.com>
2021-09-30 13:06:08 +0400

    added some proto of web ui

--- src/types.ts
+++ src/types.ts
@@ -22,3 +22,21 @@ export interface INote {
   date: Date;
   user: Types.ObjectId
 }
+
+export interface ICar {
+  lotNum: string,
+  make: string,
+  vin: string,
+  bodyStyle: string,
+  secondaryDmg: string,
+  locState: string,
+  hasKey: string,
+  repairCost: string,
+  modelGroup: string,
+  modelDetail: string,
+  engine: string,
+  damageDesc: string,
+  transmision: string,
+  imgThumb: string,
+  imgUrl: string,
+}
