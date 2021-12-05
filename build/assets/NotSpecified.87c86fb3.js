e09e0b5e99f935f1bd6a8bb0d9cd1d29b6bae447
shotius <sh.archemashvili@gmail.com>
2021-12-05 11:57:20 +0400

    uibuild

---
+++ build/assets/NotSpecified.87c86fb3.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.506dd9e8.js";import{b as t}from"./PublicLayout.69ee19e4.js";const a=()=>e.createElement(t,null);export{a as N};
--- build/assets/NotSpecified.ef22cb6d.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./vendor.fc5c11f9.js";import{b as t}from"./PublicLayout.6465b02d.js";const a=()=>e.createElement(t,null);export{a as N};
