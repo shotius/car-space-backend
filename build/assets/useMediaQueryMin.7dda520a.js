4133bfc3eb6b10df678a14a671385b19c596c799
shotius <sh.archemashvili@gmail.com>
2022-02-01 11:30:14 +0400

    uibuild

--- build/assets/useMediaQueryMin.7dda520a.js
+++
@@ -1,1 +0,0 @@
-import{ae as i}from"./index.c7e2f3f1.js";const a=e=>{const[r]=i(`(min-width: ${e}px)`);return{isLargerThan:r}};export{a as u};
---
+++ build/assets/useMediaQueryMin.adba715e.js
@@ -0,0 +1,1 @@
+import{ae as r}from"./index.ca6f01af.js";const s=e=>{const[a]=r(`(min-width: ${e}px)`);return{isLargerThan:a}};export{s as u};
