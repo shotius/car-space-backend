b08dbb2f1b824af4744b1142ed3c07e97d18e7c0
shotius <sh.archemashvili@gmail.com>
2022-02-03 21:39:11 +0400

    uibuild

--- build/assets/spacer.8d6f440a.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e}from"./index.db8f83f8.js";var r=e("div",{baseStyle:{flex:1,justifySelf:"stretch",alignSelf:"stretch"}});export{r as S};
--- build/assets/useMediaQueryMin.da0a4de7.js
+++
@@ -1,1 +0,0 @@
-import{ae as i}from"./index.db8f83f8.js";const a=e=>{const[r]=i(`(min-width: ${e}px)`);return{isLargerThan:r}};export{a as u};
