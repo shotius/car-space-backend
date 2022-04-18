f3642f311ea572f76884c0d0c2542faf7aa0cf40
shotius <sh.archemashvili@gmail.com>
2022-04-18 22:25:10 +0400

    uibuild

---
+++ build/assets/useMediaQueryMin.7f9eb745.js
@@ -0,0 +1,1 @@
+import{as as s}from"./index.632ccf89.js";const i=e=>{const[r]=s(`(min-width: ${e}px)`);return{isLargerThan:r}};export{i as u};
--- build/assets/useMediaQueryMin.ef9d73f8.js
+++
@@ -1,1 +0,0 @@
-import{as as s}from"./index.2dfc81ff.js";const i=e=>{const[r]=s(`(min-width: ${e}px)`);return{isLargerThan:r}};export{i as u};
