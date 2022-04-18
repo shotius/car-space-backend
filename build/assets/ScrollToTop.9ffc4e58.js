fce2c02ddda231d581e9cdd3ed952a508074c13a
shotius <sh.archemashvili@gmail.com>
2022-04-18 22:32:28 +0400

    uibuild

--- build/assets/ScrollToTop.9ffc4e58.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index.632ccf89.js";const t=({})=>(o.exports.useEffect(()=>window.scrollTo(0,0),[]),null);export{t as S};
---
+++ build/assets/ScrollToTop.e017ef43.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index.45177c5a.js";const t=({})=>(o.exports.useEffect(()=>window.scrollTo(0,0),[]),null);export{t as S};
