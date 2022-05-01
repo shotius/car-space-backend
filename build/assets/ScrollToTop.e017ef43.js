e3e537603189789ca4e01ac7b58f7115f32ae760
shotius <sh.archemashvili@gmail.com>
2022-05-01 19:36:30 +0400

    uibuild

--- build/assets/ScrollToTop.e017ef43.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index.45177c5a.js";const t=({})=>(o.exports.useEffect(()=>window.scrollTo(0,0),[]),null);export{t as S};
--- build/assets/useMediaQueryMin.5305f7c8.js
+++
@@ -1,1 +0,0 @@
-import{as as s}from"./index.45177c5a.js";const i=e=>{const[r]=s(`(min-width: ${e}px)`);return{isLargerThan:r}};export{i as u};
